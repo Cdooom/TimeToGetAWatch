95687868d016d5714f14ac757eb2e4328f08a0aa
Caroline Doan <caroline.doan@glidewelldental.com>
2020-03-31 16:06:38 -0700

    Changed dockerfile to TimeToGetAWatch. Changed TestController

---
+++ ItsTestingTime/Controllers/TestController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ItsTestingTime.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+
+        // GET api/test/{times}
+        [HttpGet("{times}")]
+        public ActionResult<string> Get(int times)
+        {
+            return "value";
+        }
+
+    }
+}
